7d6fcb6ce093841e6e364222f5904058e31346ef
Winson Gin <35970210+winsongin@users.noreply.github.com>
2020-05-15 21:48:47 -0700

    Updated state info shown on marker

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 import React from "react";
 import Helmet from "react-helmet";
 import L from "leaflet";
-import location from "./location.json";
+import current from "./current.json";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
@@ -21,39 +21,29 @@ const IndexPage = () => {
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
-
   async function mapEffect({ leafletElement: map } = {}) {
-    let response;
+    // let response;
 
-    try {
-      response = await axios.get(
-        "https://covidtracking.com/api/v1/states/current.json"
-      );
-    } catch (e) {
-      console.log(`Failed to fetch states: ${e.message}`, e);
-      return;
-    }
+    // try {
+    //   response = await axios.get(
+    //     "https://covidtracking.com/api/v1/states/current.json"
+    //   );
+    // } catch (e) {
+    //   console.log(`Failed to fetch states: ${e.message}`, e);
+    //   return;
+    // }
 
-    const { data = [] } = response;
-
-    const Data = Array.isArray(location) && location.length > 0;
-    if (!Data) {
-      return;
-    }
-
-    console.log(location);
-
-    const hasData = Array.isArray(data) && data.length > 0;
-    if (!hasData) {
-      return;
-    }
-    console.log(data);
+    // const { data = [] } = response;
+    // const hasData = Array.isArray(data) && data.length > 0;
+    // if (!hasData) {
+    //   return;
+    // }
+    // console.log(data);
 
     const geoJson = {
       type: "FeatureCollection",
-      features: location.map((state = {}) => {
-        const { stateInfo = {} } = state;
-        const { latitude: lat, longitude: lng } = stateInfo;
+      features: current.map((state = {}) => {
+        const { latitude: lat, longitude: lng } = state;
         return {
           type: "Feature",
           properties: {
@@ -67,7 +57,7 @@ const IndexPage = () => {
       }),
     };
 
-    console.log(data);
+    console.log(geoJson);
 
     const geoJsonLayers = new L.GeoJSON(JSON.parse(JSON.stringify(geoJson)), {
       pointToLayer: (feature = {}, latlng) => {
@@ -75,15 +65,21 @@ const IndexPage = () => {
         let updatedFormatted;
         let casesString;
 
-        const { state, lastModified, positive, death, recovered } = properties;
+        const {
+          grade,
+          state,
+          positive,
+          negative,
+          death,
+          recovered,
+          hospitalizedCurrently,
+          totalTestResults,
+          onVentilatorCurrently,
+        } = properties;
 
         casesString = `${positive}`;
         if (positive > 1000) {
           casesString = `${casesString.slice(0, -3)}k+`;
-        }
-
-        if (lastModified) {
-          updatedFormatted = new Date(lastModified).toLocaleString();
         }
 
         const html = `
@@ -91,10 +87,14 @@ const IndexPage = () => {
             <span class="icon-marker-tooltip">
               <h2>${state}</h2>
               <ul>
+                <li><strong>Grade:</strong> ${grade}</li>
                 <li><strong>Positive:</strong> ${positive}</li>
-                <li><strong>Deaths:</strong> ${death}</li>
+                <li><strong>Negative:</strong> ${negative}</li>
+                <li><strong>Death:</strong> ${death}</li>
                 <li><strong>Recovered:</strong> ${recovered}</li>
-                <li><strong>Last Modified:</strong> ${lastModified}</li>
+                <li><strong>Hospitalized Currently:</strong> ${hospitalizedCurrently}</li>
+                <li><strong>Total Test Results:</strong> ${totalTestResults}</li>
+                <li><strong>Currently on Ventilator:</strong> ${onVentilatorCurrently}</li>
               </ul>
             </span>
             ${casesString}
@@ -146,148 +146,3 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
-
-// import axios from "axios";
-// import React from "react";
-// import Helmet from "react-helmet";
-// import L from "leaflet";
-
-// import Layout from "components/Layout";
-// import Container from "components/Container";
-// import Map from "components/Map";
-
-// const LOCATION = {
-//   lat: 0,
-//   lng: 0,
-// };
-// const CENTER = [LOCATION.lat, LOCATION.lng];
-// const DEFAULT_ZOOM = 2;
-
-// const IndexPage = () => {
-//   /**
-//    * mapEffect
-//    * @description Fires a callback once the page renders
-//    * @example Here this is and example of being used to zoom in and set a popup on load
-//    */
-
-//   async function mapEffect({ leafletElement: map } = {}) {
-//     let response;
-
-//     try {
-//       response = await axios.get("https://corona.lmao.ninja/v2/countries");
-//     } catch (e) {
-//       console.log(`Failed to fetch countries: ${e.message}`, e);
-//       return;
-//     }
-
-//     const { data = [] } = response;
-
-//     const hasData = Array.isArray(data) && data.length > 0;
-//     if (!hasData) {
-//       return;
-//     }
-//     console.log(data);
-
-//     const geoJson = {
-//       type: "FeatureCollection",
-//       features: data.map((country = {}) => {
-//         const { countryInfo = {} } = country;
-//         const { lat, long: lng } = countryInfo;
-//         // testing longitude and latitude output
-//         console.log("This is latitude: ");
-//         console.log(lat);
-//         console.log("This is longitude");
-//         console.log(lng);
-
-//         return {
-//           type: "Feature",
-//           properties: {
-//             ...country,
-//           },
-//           geometry: {
-//             type: "Point",
-//             coordinates: [lng, lat],
-//           },
-//         };
-//       }),
-//     };
-
-//     console.log(geoJson);
-
-//     const geoJsonLayers = new L.GeoJSON(geoJson, {
-//       pointToLayer: (feature = {}, latlng) => {
-//         const { properties = {} } = feature;
-//         let updatedFormatted;
-//         let casesString;
-
-//         const { country, updated, cases, deaths, recovered } = properties;
-
-//         casesString = `${cases}`;
-//         if (cases > 1000) {
-//           casesString = `${casesString.slice(0, -3)}k+`;
-//         }
-
-//         if (updated) {
-//           updatedFormatted = new Date(updated).toLocaleString();
-//         }
-
-//         const html = `
-//           <span class="icon-marker">
-//             <span class="icon-marker-tooltip">
-//               <h2>${country}</h2>
-//               <ul>
-//                 <li><strong>Confirmed:</strong> ${cases}</li>
-//                 <li><strong>Deaths:</strong> ${deaths}</li>
-//                 <li><strong>Recovered:</strong> ${recovered}</li>
-//                 <li><strong>Last Update:</strong> ${updatedFormatted}</li>
-//               </ul>
-//             </span>
-//             ${casesString}
-//           </span>
-//         `;
-
-//         return L.marker(latlng, {
-//           icon: L.divIcon({
-//             className: "icon",
-//             html,
-//           }),
-//           riseOnHover: true,
-//         });
-//       },
-//     });
-//     geoJsonLayers.addTo(map);
-//   }
-
-//   const mapSettings = {
-//     center: CENTER,
-//     defaultBaseMap: "OpenStreetMap",
-//     zoom: DEFAULT_ZOOM,
-//     mapEffect,
-//   };
-
-//   return (
-//     <Layout pageName="home">
-//       <Helmet>
-//         <title>Home Page</title>
-//       </Helmet>
-
-//       <Map {...mapSettings}></Map>
-
-//       <Container type="content" className="text-center home-start">
-//         <h2>Still Getting Started?</h2>
-//         <p>Run the following in your terminal!</p>
-//         <pre>
-//           <code>
-//             gatsby new [directory]
-//             https://github.com/colbyfayock/gatsby-starter-leaflet
-//           </code>
-//         </pre>
-//         <p className="note">
-//           Note: Gatsby CLI required globally for the above command
-//         </p>
-//       </Container>
-//     </Layout>
-//   );
-// };
-
-// export default IndexPage;
